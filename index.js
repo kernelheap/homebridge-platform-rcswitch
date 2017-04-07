b2e12e7da5aa9abd4d1e4126fe88c33dc33d552c
Venkatram Tummala <venkatram867@gmail.com>
2017-04-07 23:53:25 +0000

    Adding support for toggle style RC switches

--- index.js
+++ index.js
@@ -34,6 +34,10 @@ RCSwitchPlatform.prototype.accessories = function(callback) {
     self.config.contact.forEach(function(sw) {
 	self.accessories.push(new RCContactAccessory(sw, self.log, self.config));
     });
+    self.config.toggle.forEach(function(sw) {
+    	self.accessories.push(new RCToggleAccessory(sw, self.log, self.config));
+    });
+
     setTimeout(self.listen.bind(self),10);
     callback(self.accessories);
 }
@@ -115,6 +119,57 @@ RCContactAccessory.prototype.notify = function(code) {
     }
 }
 RCContactAccessory.prototype.getServices = function() {
+    var self = this;
+    var services = [];
+    var service = new Service.AccessoryInformation();
+    service.setCharacteristic(Characteristic.Name, self.name)
+        .setCharacteristic(Characteristic.Manufacturer, 'Raspberry Pi')
+        .setCharacteristic(Characteristic.Model, 'Raspberry Pi')
+        .setCharacteristic(Characteristic.SerialNumber, 'Raspberry Pi')
+        .setCharacteristic(Characteristic.FirmwareRevision, '1.0.0')
+        .setCharacteristic(Characteristic.HardwareRevision, '1.0.0');
+    services.push(service);
+    services.push(self.service);
+    return services;
+}
+
+function RCToggleAccessory(sw, log, config) {
+    var self = this;
+    self.name = sw.name;
+    self.sw = sw;
+    self.log = log;
+    self.config = config;
+    self.currentState = false;
+    self.Timer;
+
+    self.service = new Service.Switch(self.name);
+    self.service.getCharacteristic(Characteristic.On)
+	    .on('set', self._setOn.bind(self));
+}
+
+RCToggleAccessory.prototype._setOn = function(on, callback) {
+
+	var self = this;
+	self.log("Setting switch to " + on);
+
+	if (on) {
+          rsswitch.send(self.config.send_pin, self.sw.code, self.sw.pulse);
+		if (self.sw.timeout != null) {
+		setTimeout(function() {
+		    self.service.setCharacteristic(Characteristic.On, false);
+		}.bind(self), self.sw.timeout);
+		}
+	} else if (self.sw.timeout == null) {
+          rsswitch.send(self.config.send_pin, self.sw.code, self.sw.pulse);
+	}
+
+	callback();
+}
+
+RCToggleAccessory.prototype.notify = function(code) {
+}
+
+RCToggleAccessory.prototype.getServices = function() {
     var self = this;
     var services = [];
     var service = new Service.AccessoryInformation();
